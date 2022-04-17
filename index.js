a57a512fa99fee1c77bad9cb9fec53b796c3ac89
Gerard Albajar <gerard.albajar@gmail.com>
2022-04-17 03:50:05 +0200

    Js updated and HTML and SCSS Structure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const columns = 30;
-const rows = 20;
+const rows = 30;
 const grid = [];
 
 const generateGrid = () => {
@@ -20,14 +20,14 @@ generateGrid();
 const checkingNeighbours = () => {
   for (let i = 0; i < grid.length; i++) {
     grid[i].idx = i;
-    grid[i].topIdx = i + columns;
-    grid[i].bottomIdx = i - columns;
+    grid[i].topIdx = i - columns;
+    grid[i].bottomIdx = i + columns;
     grid[i].rightIdx = i + 1;
     grid[i].leftIdx = i - 1;
-    grid[i].topRightIdx = i + columns + 1;
-    grid[i].topLeftIdx = i + columns - 1;
-    grid[i].bottomRightIdx = i - columns + 1;
-    grid[i].bottomLeftIdx = i - columns - 1;
+    grid[i].topRightIdx = i - columns + 1;
+    grid[i].topLeftIdx = i - columns - 1;
+    grid[i].bottomRightIdx = i + columns + 1;
+    grid[i].bottomLeftIdx = i + columns - 1;
   }
 };
 
@@ -35,28 +35,28 @@ checkingNeighbours();
 
 const checkIsAlive = (cell) => {
   let neighboursAliveCount = 0;
-  if (grid[cell.topLeftIdx].alive) {
+  if (grid[cell.topLeftIdx] && grid[cell.topLeftIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.topIdx].alive) {
+  if (grid[cell.topIdx] && grid[cell.topIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.topRightIdx].alive) {
+  if (grid[cell.topRightIdx] && grid[cell.topRightIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.leftIdx].alive) {
+  if (grid[cell.leftIdx] && grid[cell.leftIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.rightIdx].alive) {
+  if (grid[cell.rightIdx] && grid[cell.rightIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.bottomLeftIdx].alive) {
+  if (grid[cell.bottomLeftIdx] && grid[cell.bottomLeftIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.bottomIdx].alive) {
+  if (grid[cell.bottomIdx] && grid[cell.bottomIdx].alive) {
     neighboursAliveCount++;
   }
-  if (grid[cell.bottomRightIdx].alive) {
+  if (grid[cell.bottomRightIdx] && grid[cell.bottomRightIdx].alive) {
     neighboursAliveCount++;
   }
 
@@ -66,18 +66,34 @@ const checkIsAlive = (cell) => {
   return false;
 };
 
+const updateAlive = () => {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].alive = checkIsAlive(grid[i]);
+  }
+};
+
+// eslint-disable-next-line no-unused-vars
+const startGame = () => {
+  updateAlive();
+};
+
+const selectCell = (cell) => {
+  const myCell = cell.path[0];
+  const idCell = Number(myCell.id);
+  myCell.classList.add("selected");
+  grid[idCell].alive = true;
+};
+
 function renderGrid() {
   const mainContainer = document.getElementById("grid-container");
-  let index = 0;
   for (let i = 0; i < rows; i++) {
     const newRow = document.createElement("div");
     newRow.classList.add("row");
     for (let j = 0; j < columns; j++) {
       const newColumn = document.createElement("div");
       newColumn.classList.add("cell");
-      newColumn.id = `${index}`;
+      newColumn.onclick = selectCell;
       newRow.appendChild(newColumn);
-      index++;
     }
     mainContainer.appendChild(newRow);
   }
